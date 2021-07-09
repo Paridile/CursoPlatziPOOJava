a2f8429c434767b2a2571fc0df4d5c27a224333c
Pablo Díaz de León <47725184+Paridile@users.noreply.github.com>
2021-07-09 10:50:23 -0500

    Clase 12 variable vs objeto

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ import static UI.UIMenu.*;
 public class Main {
     public static void main(String[] args) {
         showMenu();
+        Patient patient1 = new Patient("Pablo","Rios");
+        Patient patient2 = new Patient("Nombre","Apellido");
+        System.out.println(patient1.name);
+        System.out.println(patient2.name);
+        patient2 = patient;
+        System.out.println(patient1.name);
+        System.out.println(patient2.name);
     }
 }
