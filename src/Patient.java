604b8b6635231ea1ea0659b2640fe19c78c55cf4
Pablo Díaz de León <47725184+Paridile@users.noreply.github.com>
2021-07-09 10:30:59 -0500

    Clase 10 reto

---
+++ src/Patient.java
@@ -0,0 +1,14 @@
+public class Patient {
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthdy;
+    double weight;
+    double height;
+    String blood;
+    public Patient(String name,String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
