154a66a1669f66b4aaa18ef85edb6319cbe4af68
“Pablo <pablorios1603@gmail.com>
2021-07-09 11:51:34 -0500

    Clase 15 variable vs objeto

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import javax.print.Doc;
+
 import static UI.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
+        Doctor doctor = new Doctor("Pablo", "Java");
         //showMenu();
+        int i = 0;
+        String name = "Pablo";
+
         Patient patient1 = new Patient("Pablo","Rios");
         Patient patient2 = new Patient("Nombre","Apellido");
         System.out.println(patient1.getName());
